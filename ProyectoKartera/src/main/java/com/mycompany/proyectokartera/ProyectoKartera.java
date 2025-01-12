8544237d1b3c38b392d241a3c00d23f05979f171
Tomas Aristizabal Gomez <tomasaristizabalgomez@gmail.com>
2025-01-11 21:28:39 -0500

    cambios

---
+++ ProyectoKartera/src/main/java/com/mycompany/proyectokartera/ProyectoKartera.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectokartera;
+
+/**
+ *
+ * @author tomas
+ */
+public class ProyectoKartera {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
